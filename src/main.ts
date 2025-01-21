68ed5d6fdb3cf3b693708de2e8e8270ef266813d
Mariem BenYounes <maryemby21@gmail.com>
2025-01-21 19:12:41 +0100

    improved complexity and added clear button

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,33 @@ const items_per_line=60
 const obstacles_per_line=20
 let user_start=false
 let user_end=false
-
+let start_node:Cell;
+let end_node:Cell;
 
 type Cell =HTMLElement | null | undefined
-type Parentiality= {
-parent: HTMLElement | null | undefined
-current: HTMLElement | null | undefined
+
+interface visitedNode {
+    parent: Cell
+    current: Cell
+}
+
+enum SerachType{
+    BFS='BFS',
+    DFS='DFS'
+}
+
+interface Stats{
+    visited:number,
+    time:string
+}
+
+function getNextNode(queue:Cell[],type:SerachType){
+    if(type===SerachType.BFS){
+        return queue.shift()
+    }
+    if(type===SerachType.DFS){
+        return queue.pop()
+    }
 }
 
 function createGrid(){
@@ -34,57 +55,26 @@ function addObstacles(){
 }
 }
 
-function handleStartingNodes(){
-    if(!user_start){
-        for (let i=0;i<grid_items;i++){
-            let item=document.getElementById(i.toString());
-            if(item?.style.backgroundColor!=='green'){
-                item?.addEventListener('click',()=>{
-                    item?.classList.add('start');
-                })
-            }
-
-    }
-    user_start = true;
-    return ;
-}
-    if(!user_end){
-        for (let i=0;i<grid_items;i++){
-            let item=document.getElementById(i.toString());
-            if(item?.style.backgroundColor!=='green'){
-                item?.addEventListener('click',()=>{
-                    item?.classList.add('end');
-                })
-            }
-        }
-        user_end=true;
-        return;
-    }
-    if(user_start && user_end){
-        for(let i=0;i<grid_items;i++){
-            let item=document.getElementById(i.toString());
-            item?.removeEventListener('click',()=>{});
-        }
-    }
-}
-
 function addNodes(){
     for(let i=0;i<grid_items;i++){
         let item=document.getElementById(i.toString());
         if (!item) continue
-        const currentColor = getComputedStyle(item).backgroundColor;
         item?.addEventListener('click',()=>{
-            if(currentColor!=="rgb(0, 128, 0)"){
+            if(!item.classList.contains('obstacle')){
                 if(!user_start){
                     item.classList.add('start');
                     user_start=true;
+                    start_node=item;
                     return;
                 }
                 if(user_start && !user_end){
                     item.classList.add('end');
                     user_end=true;
+                    end_node=item;
                     const button = document.getElementById('bfs') as HTMLButtonElement;
-                    button.addEventListener('click', BFS);
+                    const button2 = document.getElementById('dfs') as HTMLButtonElement;
+                    button.addEventListener('click', () => FS(SerachType.BFS));
+                    button2.addEventListener('click', () => FS(SerachType.DFS));
                     return;
                 }
         }
@@ -93,89 +83,96 @@ function addNodes(){
     }
 }
 
-function BFS(){
-    console.log('BFS')
-    let visited:Parentiality[] = [];
-    //for fast retrieval
-    let visitedSet = new Set<Cell>();
-    let queue=[]
-    let path: Cell[] = []
-    let rel=[];
-    let parent;
+function handleUI(){
+    const clearButton=document.getElementById('clear') as HTMLButtonElement;
+    clearButton.addEventListener('click',()=>{
+    clearGrid();
+})
+}
 
-    //pushing starting item
-    for(let i=0;i<grid_items;i++){
-        let item=document.getElementById(i.toString());
-        if(item?.classList.contains('start')){
-            queue.push(item);
-        }
+function getNeighbors(cell:Cell):Cell[]{
+    let neighbors:Cell[]=[];
+    let id = parseInt(cell?.id || '');
+    if (!isNaN(id) && id - items_per_line >= 0) {
+        neighbors.push(document.getElementById((id-items_per_line).toString()));
     }
-    visited.push(
-        {
-            parent: null,
-            current: queue[0]
-        }
-    )
-    visitedSet.add(queue[0])
+    if (!isNaN(id) && id + items_per_line <= grid_items-1) {
+        neighbors.push(document.getElementById((id+items_per_line).toString()));
+    }
+    if (!isNaN(id) && id%items_per_line!=0) {
+        neighbors.push(document.getElementById((id-1).toString()));
+    }
+    if (!isNaN(id) && (id+1)%items_per_line!=0) {
+        neighbors.push(document.getElementById((id+1).toString()));
+    }
+    return neighbors;
+}
+
+function FS(type: SerachType){
+    const start = performance.now();
+    let visited:Map<string,visitedNode> = new Map();
+    let queue:Cell[]=[]
+    let path: Cell[]=[]
+    let stats:Stats;
+   if(!start_node){
+    console.log('No start node')
+    return
+   }
+    queue.push(start_node);
+    visited.set(start_node.id, {
+        parent: null,
+        current: start_node
+    });
     while(queue.length>0){
-        let neighbors=[]
-        const current=queue.shift();
-        if(rel.length>0){
-            for(let i=0;i<rel.length;i++){
-                if(current && rel[i].neighbors.includes(current)){
-                    parent=rel[i].parent;
-                    break;
-                }
-        }
-        visited.push({
-            parent: parent,
-            current: current
-        })
-    }
+        let neighbors:Cell[]=[]
+        const current=getNextNode(queue,type);
         if (current?.classList.contains('end')){
+
+            let node:visitedNode | undefined = visited.get(current.id);
+            while(node?.parent){
+                path.unshift(node.parent);
+                node=visited.get(node.parent.id)
+            }
             break;
         }
-        visitedSet = new Set(visited.map(v => v.current))
+        neighbors=getNeighbors(current)
 
-        let id = parseInt(current?.id || '');
-        if (!isNaN(id) && id - items_per_line >= 0) {
-            neighbors.push(document.getElementById((id-60).toString()));
-        }
-        if (!isNaN(id) && id + items_per_line <= grid_items-1) {
-            neighbors.push(document.getElementById((id+60).toString()));
-        }
-        if (!isNaN(id) && id%items_per_line!=0) {
-            neighbors.push(document.getElementById((id-1).toString()));
-        }
-        if (!isNaN(id) && (id+1)%items_per_line!=0) {
-            neighbors.push(document.getElementById((id+1).toString()));
-        }
-        for(let i=0;i<neighbors.length;i++){
-            if (!neighbors[i]?.classList.contains('obstacle') && !neighbors[i]?.classList.contains('start') && !visitedSet.has(neighbors[i])) {
-                queue.push(neighbors[i]);
+        for(const neighbor of neighbors){
+            if (neighbor && !neighbor?.classList.contains('obstacle') && !neighbor?.classList.contains('start') && !visited.has(neighbor.id)) {
+                visited.set(neighbor.id, {
+                    parent: current,
+                    current: neighbor
+                });
+                queue.push(neighbor);
             }
-        }
-       rel.push({
-        parent:current,
-        neighbors:neighbors
-       })
-        
+        } 
     }
-    console.log(rel)
-    //finding path
-    let i=visited.length-1  
-    while(visited[i].parent){
-        console.log(`Parent: ${visited[i].parent?.id} of current: ${visited[i].current?.id}`);
-        path.push(visited[i].parent);
-        i=visited.findIndex(v => v.current === visited[i].parent);
+    const end = performance.now();
+    stats={
+        visited:visited.size,
+        time: (end-start).toFixed(2)+"ms"
     }
-    //adding classes
-    for(let i=1;i<visited.length-1;i++){
-        visited[i].current?.classList.add('visited')
+    console.log(stats);
+    for(const node of visited.values()){
+        if(node.current?.classList.contains('start')) continue;
+        if(node.current?.classList.contains('end')) continue;
+        node.current?.classList.add('visited')
     }
-    for(let i=0;i<path.length-1;i++){
-        path[i]?.classList.add('path')
+
+    for(const node of path){
+        if(node?.classList.contains('start')) continue;
+        node?.classList.add('path')
     }
+    
+}
+
+function clearGrid(){
+    start_node?.classList.remove('start');
+    end_node?.classList.remove('end');
+    start_node = null;
+    end_node = null;
+    user_start=false;
+    user_end=false
 }
 
 createGrid();
